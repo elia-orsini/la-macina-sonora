2fcadc80cd4be161c85ba69705c8ddc4f903ea84
Your name here <elia.orsini@hotmail.com>
2025-04-05 09:02:35 +0100

    chore: capitalize metadata

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -54,32 +54,32 @@ export async function generateMetadata({ params }: { params: Promise<{ slug: str
 
   switch (slug) {
     case "the-studio":
-      title = "the studio";
+      title = "The Studio | La Macina Sonora";
       description =
         "La Macina Sonora nasce dall'idea di Rino Bellandi di realizzare uno studio di registrazione e produzione musicale in grado di unire l'arte della registrazione tradizionale con le moderne tecniche di produzione audio.";
       break;
-    case "recordings":
+    case "Recordings | La Macina Sonora":
       title = "recordings";
       description =
         "La Macina Sonora è divisa in due stanze, ideate e realizzate con due diversi approcci. Entrambe sono state progettate e trattate acusticamente da Riccardo Buccelleni, ingegnere specializzato nell'acustica degli ambienti che ha curato nei minimi dettagli il suono e l'estetica delle due stanze per poter realizzare al meglio ogni idea artistica.";
       break;
     case "productions":
-      title = "productions";
+      title = "Productions | La Macina Sonora";
       description =
         "Il team audio della La Macina Sonora è composto da diversi producer e musicisti.";
       break;
     case "soundsystem":
-      title = "soundsystem";
+      title = "Soundsystem | La Macina Sonora";
       description =
         "La Macina Sonora Soundsystem nasce dall'idea di portare il suono del nostro studio anche nel mondo Live affiancando gli artisti e i professionisti che hanno scelto di affidarsi al nostro team per la propria musica.";
       break;
     case "events":
-      title = "events";
+      title = "Events | La Macina Sonora";
       description =
         "La Macina Eventi è la divisione Live dello studio di registrazione e produzione La Macina Sonora. L'agenzia nasce dall'idea di raccogliere diverse figure professionali del settore dell'intrattenimento in grado di curare a 360° l'organizzazione di un evento pubblico e/o privato.";
       break;
     case "contacts":
-      title = "contacts";
+      title = "Contacts | La Macina Sonora";
       description = "Contatta La Macina Sonora per Email, su Instagram o su Facebook.";
       break;
     default:
