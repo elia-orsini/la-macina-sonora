ea810be620a34a10d1c84982a657ebe03fb8078f
Your name here <elia.orsini@hotmail.com>
2025-04-05 08:58:59 +0100

    chore: update metadata

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -37,3 +37,49 @@ export default async function SlugPage({ params }: { params: Promise<{ slug: str
     </main>
   );
 }
+
+export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }) {
+  const slug = (await params).slug;
+
+  let title = "";
+  let description = "";
+
+  switch (slug) {
+    case "the-studio":
+      title = "the studio";
+      description =
+        "La Macina Sonora nasce dall'idea di Rino Bellandi di realizzare uno studio di registrazione e produzione musicale in grado di unire l'arte della registrazione tradizionale con le moderne tecniche di produzione audio.";
+      break;
+    case "recordings":
+      title = "recordings";
+      description =
+        "La Macina Sonora è divisa in due stanze, ideate e realizzate con due diversi approcci. Entrambe sono state progettate e trattate acusticamente da Riccardo Buccelleni, ingegnere specializzato nell'acustica degli ambienti che ha curato nei minimi dettagli il suono e l'estetica delle due stanze per poter realizzare al meglio ogni idea artistica.";
+      break;
+    case "productions":
+      title = "productions";
+      description =
+        "Il team audio della La Macina Sonora è composto da diversi producer e musicisti.";
+      break;
+    case "soundsystem":
+      title = "soundsystem";
+      description =
+        "La Macina Sonora Soundsystem nasce dall'idea di portare il suono del nostro studio anche nel mondo Live affiancando gli artisti e i professionisti che hanno scelto di affidarsi al nostro team per la propria musica.";
+      break;
+    case "events":
+      title = "events";
+      description =
+        "La Macina Eventi è la divisione Live dello studio di registrazione e produzione La Macina Sonora. L'agenzia nasce dall'idea di raccogliere diverse figure professionali del settore dell'intrattenimento in grado di curare a 360° l'organizzazione di un evento pubblico e/o privato.";
+      break;
+    case "contacts":
+      title = "contacts";
+      description = "Contatta La Macina Sonora per Email, su Instagram o su Facebook.";
+      break;
+    default:
+      break;
+  }
+
+  return {
+    title: title,
+    description: description,
+  };
+}
